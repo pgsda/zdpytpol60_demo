e50f63d516936600b9bf53663b80696ff1b9ed43
Piotr <trenersda@gmail.com>
2023-04-16 10:06:39 +0200

    feature(001) Allow to add new user

---
+++ add_new_user.py
@@ -0,0 +1,10 @@
+users = []
+
+user = input('Podaj nazwę użytkownika: ')
+
+users.append(user)
+
+print(f'Użytkownik  {user} został dodany do bazts danych')
+
+print('Wszyscy użytkownicy: ')
+print(users)
