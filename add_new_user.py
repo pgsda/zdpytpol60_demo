5da01cbbf62a73d8a8287eb20be4662ccf763764
pgsda <59748059+pgsda@users.noreply.github.com>
2023-04-16 10:27:50 +0200

    feature(001) Allow to add new user (#1)
    * feature(001) Allow to add new user
    
    * feature(001) Fix typos

---
+++ add_new_user.py
@@ -0,0 +1,10 @@
+users = []
+
+user = input('Podaj nazwę użytkownika: ')
+
+users.append(user)
+
+print(f'Użytkownik {user} został dodany do bazy danych')
+
+print('Wszyscy użytkownicy: ')
+print(users)
